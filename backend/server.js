0af38f943e2e6d8ffe4d4e793f0fbf2b3b0c2177
sarinjet1008 <sarinjetley@gmail.com>
2025-04-11 10:00:04 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,1 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-// Route Imports
-const userRoutes = require('./routes/users');
-const recipeRoutes = require('./routes/recipes');
-
-// Mount Routes
-app.use('/api/users', userRoutes);
-app.use('/api/recipes', recipeRoutes);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
